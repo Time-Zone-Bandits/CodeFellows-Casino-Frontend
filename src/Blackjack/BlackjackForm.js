f55d8e9058e5be61a0b07a50702f4659b8d00487
Mandy Mason <mnmason86@gmail.com>
2022-08-02 13:42:14 -0400

    setup component skeletons

---
+++ src/Blackjack/BlackjackForm.js
@@ -0,0 +1,26 @@
+// 'use strict';
+import {Component} from 'react';
+import axios from 'axios';
+
+
+
+class BlackjackForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <>
+      
+      
+      </>
+    )
+  }
+}
+
+export default BlackjackForm;
---
+++ src/Blackjack/BlackjackUser.js
@@ -0,0 +1,28 @@
+// 'use strict';
+import {Component} from 'react';
+import axios from 'axios';
+
+
+
+class BlackjackUser extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <>
+      
+      
+      </>
+    )
+  }
+
+
+}
+
+export default BlackjackUser;
